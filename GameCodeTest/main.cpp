1e251a7e2d9404295fbac0252f8436a1af13bfa5
Jinho Yoo <jinhoyoo@gamebase.co.kr>
2014-10-07 17:02:08 +0900

    Initial Commit

---
+++ GameCodeTest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GameCodeTest
+//
+//  Created by Jinho Yoo on 10/7/14.
+//  Copyright (c) 2014 Jinho Yoo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
