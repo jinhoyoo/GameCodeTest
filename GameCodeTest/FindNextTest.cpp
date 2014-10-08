37f6fcf2c115660dfe82c6b4df3ff00598832fcc
Jinho Yoo <jinhoyoo@gamebase.co.kr>
2014-10-08 15:34:12 +0900

    Added find next optimization and test routine.

---
+++ GameCodeTest/FindNextTest.cpp
@@ -0,0 +1,84 @@
+//
+//  FindNext.cpp
+//  GameCodeTest
+//
+//  Created by Jinho Yoo on 10/8/14.
+//  Copyright (c) 2014 Jinho Yoo. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "gtest/gtest.h"
+
+std::string FindNext(std::vector<std::string> items, std::string current)
+{
+    bool returnNext = false;
+    
+    for (auto item : items){
+        
+        if (returnNext)
+            return item;
+        else if (item == current)
+            returnNext = true;
+    }
+    return "";
+}
+
+std::string OptimizedFindNext(std::vector<std::string> items, std::string current)
+{
+    bool returnNext = false;
+    
+    for (auto const &item : items){
+        
+        if (returnNext)
+            return item;
+        else if (item == current)
+            returnNext = true;
+    }
+    return "";
+}
+
+//Test fixture
+class FindNextTest : public testing::Test {
+protected:
+    virtual void SetUp() {
+        _stringArray.reserve(15);
+        _stringArray.push_back("Pages");
+        _stringArray.push_back("you");
+        _stringArray.push_back("view");
+        _stringArray.push_back("in");
+        _stringArray.push_back("incognito");
+        _stringArray.push_back("tabs");
+        _stringArray.push_back("will not");
+        _stringArray.push_back("stick");
+        _stringArray.push_back("around");
+        _stringArray.push_back("in");
+        
+    }
+    
+    // virtual void TearDown() {}
+    std::vector<std::string> _stringArray;
+};
+
+
+TEST_F(FindNextTest, NormalVersion){
+    std::string nextWord
+        = FindNext( _stringArray, "in");
+    
+    EXPECT_EQ(nextWord, "incognito");
+    
+}
+
+
+
+TEST_F(FindNextTest, OptimizedVersion){
+    std::string nextWord
+        = OptimizedFindNext( _stringArray, "in");
+    
+    EXPECT_EQ(nextWord, "incognito");
+    
+}
+
+
+
